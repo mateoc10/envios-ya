b7bfed111d3eb780b449e4d73fbc4b1a2e9f2fec
mateoc10 <mateoc10@gmail.com>
2017-10-06 22:07:15 +0000

    user-layout start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "hello, world!"
+    render :template => '/../views/layouts/application.html.erb'
   end
 end
