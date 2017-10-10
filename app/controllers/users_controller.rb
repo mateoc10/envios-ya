9101ffc8d333f56724cb6a1ca1c2c5100aafb3a7
Rodrigo <erreperez2@gmail.com>
2017-10-10 00:43:13 +0000

    fixes de user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :lastName, :document, :email, :password)
     end
 end
