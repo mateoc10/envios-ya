8989605eec49fbbdf77136802a794494ac9f3b95
mateoc10 <mateoc10@gmail.com>
2017-10-11 23:00:05 +0000

    login for normal user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessor :name, :email
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root 'layout#home'
   get 'users/new'
   get  '/signup',  to: 'users#new'
   get 'layout/home'
   resources :users
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
 end
