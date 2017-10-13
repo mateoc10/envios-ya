ebf1c4bf1fde5fd5b66137d051cee753e39716b8
mateoc10 <mateoc10@gmail.com>
2017-10-13 13:02:55 +0000

    image upload for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :lastName, :document, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :lastName, :document, :email, :password, :password_confirmation, :avatar)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012230943) do
+ActiveRecord::Schema.define(version: 20171013121914) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +24,10 @@ ActiveRecord::Schema.define(version: 20171012230943) do
     t.string "lastName"
     t.string "document"
     t.string "photo"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
