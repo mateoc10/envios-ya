4f8a3b709a0d1ad744424e6be8934406b9680545
Rodrigo <erreperez2@gmail.com>
2017-10-14 18:19:22 +0000

    shipment controller

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,28 @@
 class ShipmentsController < ApplicationController
   def new
+    @shipment = Shipment.new
   end
+  
+  def create
+    @shipment = Shipment.new(shipment_params)
+    
+    @shipment.state = 'In Progress'
+    @shipment.date = Date.now
+    if @user.save
+      flash.now[:success] = "Welcome to Envios ya!"
+      # render 'new'
+    else
+      if :password != :password_confirmation 
+        flash.now[:danger] = "Passwords don't match"
+      end
+      render 'new'
+    end
+  end
+
+  private
+
+    def shipment_params
+      params.require(:shipment).permit(:price, :payment, :date, )
+    end
+  
 end
