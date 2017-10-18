5a89a52f0a486615e4989cdfdd1d4c6c533f7460
mateoc10 <mateoc10@gmail.com>
2017-10-18 00:17:52 +0000

    geocode

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -5,7 +5,9 @@ class ShipmentsController < ApplicationController
   
   def create
     @shipment = Shipment.new(shipment_params)
-    
+    origin = create_location(-34.881725, -56.073983)
+    destination = create_location(-34.927254, -56.158236)
+    byebug
     @shipment.state = 'In Progress'
     @shipment.date = DateTime.now
     if @shipment.save
@@ -24,5 +26,23 @@ class ShipmentsController < ApplicationController
     def shipment_params
       params.require(:shipment).permit(:price, :payment, :date, :driver, :destination, :sender, :receiver, :origin)
     end
-  
+    
+    def create_location(lat, lng)
+      geocode = JSON.parse Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/geocode/json?latlng="+ lat.to_s() +"," + lng.to_s() + "&key=AIzaSyCULAfBJit219O85L4mwt4nqVhBL9KARCQ"))
+      
+      addresses = geocode["results"][0]["address_components"]
+      number = ''
+      address = ''
+      zipcode = ''
+      addresses.each { |adr|
+        if adr["types"][0] == "route"
+          address = adr["long_name"]
+        elsif adr["types"][0] == "street_number"
+          number = adr["long_name"]
+        elsif adr["types"][0] == "postal_code"
+          zipcode = adr["long_name"]
+        end
+      }
+      return Location.new(lat: lat, long: lng, street: address, number: number, zipcode: zipcode)
+    end
 end
